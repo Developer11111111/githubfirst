e7a3f314da2621a8c120c3f1a9d7b3d9ec3cd2cd
Developer11111111 <70813837+Developer11111111@users.noreply.github.com>
2021-08-12 13:45:43 +0500

    update FirstGit.class

--- Test/src/FisrtGit.java
+++ Test/src/FisrtGit.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class FisrtGit {
     public static void main(String args[]){
+        Scanner cin = new Scanner(System.in);
         System.out.println("Hello world!");
     }
 }
